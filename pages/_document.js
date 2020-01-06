729ae218b43dc7cd5805b4abf1016684e261af46
Sebat Mohamed <sebat.mohamed@yahoo.com>
2020-01-06 14:54:12 -0500

    fix: tracking code

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        {/* Global site tag (gtag.js) - Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155440032-1"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)};
+          function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
+
           gtag('config', 'UA-155440032-1');
         </script>
         <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,700&display=swap" rel="stylesheet"/>
