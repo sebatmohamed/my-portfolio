263ddca72387e5117e753e817ac56e2c65697105
Sebat Mohamed <sebat.mohamed@yahoo.com>
2020-01-06 15:52:17 -0500

    fix: syntax adjusted to jsx

--- pages/_document.js
+++ pages/_document.js
@@ -13,11 +13,11 @@ class MyDocument extends Document {
         {/* Global site tag (gtag.js) - Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155440032-1"></script>
         <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+          {window.dataLayer = window.dataLayer || []};
+          {function gtag(){dataLayer.push(arguments)}};
+          {gtag('js', new Date())};
 
-          gtag('config', 'UA-155440032-1');
+          {gtag('config', 'UA-155440032-1')};}
         </script>
         <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,700&display=swap" rel="stylesheet"/>
         </Head> 
